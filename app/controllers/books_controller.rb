36ae931f77c7169cc1b38b1a10cad4ab3781438c
Equitis90 <svuitis@gmail.com>
2018-02-02 19:23:16 +0200

    Add book review creation, styling, navbar

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,4 +8,23 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+
+  def create
+    pr = book_params
+    genres = Genre.where(id: pr[:genres]).to_a
+    pr[:genres] = genres
+    Book.create(pr)
+    redirect_to root_path
+  end
+
+  def show
+    @book = Book.where(id: params[:id]).first
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :user_id, :cover,
+                                 :description, :draft, genres: [])
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,5 @@ class Book < ApplicationRecord
 
   has_attached_file :cover, styles: { medium: "300x500>", thumb: "100x200>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/
+  validates_presence_of :genres, :title, :author, :description, :draft, :user
 end
